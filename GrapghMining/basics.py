573c0bcac699de6eaaca7422251adde91b0f4482
Narendrababu S <Narendra.b.sinappa@gmail.com>
2020-08-02 18:31:00 +0530

    graph mining

---
+++ GrapghMining/basics.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+"""
+networkx is a graph mining package, it is useful to analyze the networking system and also to solve some of the graph related problems.
+
+"""
+# this is the undirected graph
+G = nx.Graph()
+G.add_node("a",)
+G.add_node("b")
+G.add_node("c")
+G.add_node("d")
+G.add_node("e")
+#add the edge explicitly
+G.add_edge("a","b")
+G.add_edge("a","e")
+G.add_edge("a","d")
+#draw method 
+nx.draw(G,with_labels = True,font_weight='bold')
+#create graph by importing the edgelist data from a txt file( data format : a b, where a and b are the nodes of a graph)
+H = nx.read_edgelist("graph_data.txt")
+nx.edges(H)
+nx.draw(H,with_labels = True,font_weight='bold')
+plt.show()
+
+#getting the degree of each node
+deg = H.degree()
+
+#................................................................................................
